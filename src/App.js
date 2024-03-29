d75919f06fbb9ad95813172f6b3eb3d44b6e2598
Pankaj Kumar <pankajsing555@gmail.com>
2024-03-29 18:26:36 +0530

    betrAI test project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ScrollBar from "./componenet/ScrollBar";
+
+function App() {
+  return (
+    <div className="App">
+      <ScrollBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componenet/ScrollBar.jsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./ScrollBar.css";
+
+const ScrollBar = () => {
+  const [data, setData] = useState([]);
+  const [pageText, setPageText] = useState(50);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.frontendexpert.io/api/fe/testimonials?limit=9&after=${pageText}`
+      );
+      setData((prevData) => [...prevData, ...response.data.testimonials]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleScroll = () => {
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >=
+        document.documentElement.scrollHeight
+      ) {
+        setPageText((prev) => prev + 9);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [pageText]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>ScrollBar</h1>
+      <div className="scroll-bar-text">
+        {data &&
+          data.map((item) => (
+            <div
+              key={item.id}
+              style={{ backgroundColor: "#28231D", borderRadius: "5px" }}
+            >
+              <p>
+                <strong>{item.id}: </strong>
+                {item.message}
+              </p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default ScrollBar;
